3463e85e3e16e7fa802ea13433ed2771b6109f05
KatyaRomashko <skairlet93@gmail.com>
2025-03-18 12:36:20 +0000

    Initial commit: Set up project structure with CDK and TypeScript

---
+++ test/serverless-movie-reviews.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessMovieReviews from '../lib/serverless-movie-reviews-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-movie-reviews-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessMovieReviews.ServerlessMovieReviewsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
